44c7a7478980faa9a46228ec56914c00ef6a8fa8
Arne <arne.kraaijeveld>
2024-03-27 10:20:33 +0100

    realiseren 6.2 bijna af

--- efteling/index.php
+++ efteling/index.php
@@ -13,7 +13,6 @@ $style = "./styles/";
 $logo = "./images/";
 ?>
 
-
 <?php
 include "./includes/Header.php";
 ?>
@@ -21,29 +20,8 @@ include "./includes/Header.php";
 include "./includes/Navigatie.php";
 ?>
 <main>
-    <!--    div voor de achtergrond foto -->
-    <div class="achtergrond">
-        <!--       titel-->
-        <h1>
-            TANDARTS DEN BOSCH
-        </h1>
-        <!--       iets kleinere tekst dan titel-->
-        <h2>
-            Uw tandarts in Den Bosch e.o. voor reguliere tandheelkunde en implantaten
-        </h2>
-        <!--       paragraaf-->
-        <p>
-            Betrouwbaar. Bereikbaar. Betaalbaar.
-        </p>
+   
 
-        <btn class="inschrijven">
-            INSCHRIJVEN
-        </btn>
-        <btn class="afspraak">
-            AFSPRAAK MAKEN
-        </btn>
-        <br>
-    </div>
 </main>
 </body>
 </html>
