2d9a1b43aa57c93b7385e1b91983a6f31c3b0c81
Jesse van der Pluijm <jesse.vanderpluijm1@edu-kw1c.nl>
2024-04-02 11:54:16 +0200

    praktijk

--- efteling/includes/Navigatie.php
+++ efteling/includes/Navigatie.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styles/stylesheet.css">
 <title>Efteling Navigatiebalk</title>
 
 </head>
--- efteling/index.php
+++ efteling/index.php
@@ -1,27 +1,39 @@
-<?php
-/**
- * Users: William Steenbergen, Jesse van der pluijm en Arne Kraaijveld
- * Date: 26-3-2024
- * File: index.php
- */
-?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De Efteling</title>
 
-<?php
-$indexNav= "";
-$vragNav = "pages/";
-$style = "./styles/";
-$logo = "./images/";
-?>
+    <link rel="stylesheet" type="text/css" href="styles/stylesheet.css">
 
-<?php
-include "./includes/Header.php";
-?>
-<?php
-include "./includes/Navigatie.php";
-?>
+</head>
+<body>
+
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <img src="images/eftelinglogo.jpg" alt="Efteling Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Ontdek het park</a></li>
+                <li><a href="#">Plan je bezoek</a></li>
+                <li><a href="#">Attracties</a></li>
+                <li><a href="#">Shows en entertainment</a></li>
+                <li><a href="#">Verblijf</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
 <main>
-   
-
+    <section class="middenknop">
+        <h1>Ontdek alle betoverende</h1>
+        <h1>attracties in de Efteling</h1>
+        <a href="#" class="button">Ontdek de Efteling</a>
+    </section>
 </main>
+
 </body>
 </html>
