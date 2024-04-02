de4ed5f3d95220700e559ad9b605a784ee3f565f
Arne <arne.kraaijeveld>
2024-04-02 10:26:09 +0200

    Praktijk plaatje bijna goed

--- efteling/index.php
+++ efteling/index.php
@@ -19,9 +19,37 @@ include "./includes/Header.php";
 <?php
 include "./includes/Navigatie.php";
 ?>
-<main>
-   
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
 
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/stylesheet.css" rel="stylesheet">
+
+</head>
+<body>
+<header>
+    <h1>
+
+    </h1>
+</header>
+
+<main id="wrapper">
+    <nav>
+        <ul>
+
+        </ul>
+    </nav>
+    <section>
+           <div id="plaatje">
+
+           </div>
+    </section>
 </main>
 </body>
 </html>
+
+
