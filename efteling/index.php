d07e05beff73b08ff9b506bffae31d20135f36f1
William Steenbergen <william.steenbergen@edu-kw1c.nl>
2024-04-03 09:45:35 +0200

    goede versie

--- efteling/index.php
+++ efteling/index.php
@@ -38,7 +38,7 @@
     <section class="middenknop">
         <h1>Ontdek alle betoverende</h1>
         <h1>attracties in de Efteling</h1>
-        <a href="#" class="button">Ontdek de Efteling</a>
+        <a href="#" class="button">Ontdek de Efteling ></a>
     </section>
 </main>
 
