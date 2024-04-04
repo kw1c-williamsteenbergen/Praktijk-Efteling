7479d28aff19e28f110468572e8f3a1404f7f1f0
Jesse van der Pluijm <jesse.vanderpluijm1@edu-kw1c.nl>
2024-04-04 11:04:32 +0200

    praktijk 3.0

--- efteling/includes/Navigatie.php
+++ efteling/includes/Navigatie.php
@@ -1,3 +1,13 @@
+<div>
+    <nav>
+        <ul>
+            <li><a href="#">Klantenservice & contact</a></li>
+            <li><a href="#">Vacatures</a></li>
+            <li><a href="#">Inloggen</a></li>
+            <li><a href="#">NL</a></li>
+        </ul>
+</div>
+
 <div class="header-container">
     <div class="logo">
         <img src="images/eftelinglogo.jpg" alt="Efteling Logo">
