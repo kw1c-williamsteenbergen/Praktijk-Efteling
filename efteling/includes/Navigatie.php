efb160ff1b4b8df80ab028f016efacdbfcb93a0e
Jesse van der Pluijm <jesse.vanderpluijm1@edu-kw1c.nl>
2024-04-04 11:41:17 +0200

    praktijk 3.0

--- efteling/includes/Navigatie.php
+++ efteling/includes/Navigatie.php
@@ -1,12 +1,4 @@
-<div>
-    <nav>
-        <ul>
-            <li><a href="#">Klantenservice & contact</a></li>
-            <li><a href="#">Vacatures</a></li>
-            <li><a href="#">Inloggen</a></li>
-            <li><a href="#">NL</a></li>
-        </ul>
-</div>
+
 
 <div class="header-container">
     <div class="logo">
@@ -17,7 +9,7 @@
             <li><a href="#">Ontdek het park</a></li>
             <li><a href="#">Plan je bezoek</a></li>
             <li><a href="#">Kom overnachten</a></li>
-            <li><a href="#">Welke atractie past bij jouw?</a></li>
+            <li><a href="./pages/vragenlijst.php">Welke atractie past bij jouw?</a></li>
             <li><a href="#" class="button2">Tickets</a></li>
         </ul>
     </nav>
