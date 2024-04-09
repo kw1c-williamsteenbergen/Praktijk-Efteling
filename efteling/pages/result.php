7939b2425d57ff520e8dc21c3ff149de1c98071d
Jesse van der Pluijm <jesse.vanderpluijm1@edu-kw1c.nl>
2024-04-09 13:44:48 +0200

    praktijk af alleen commentaar

--- efteling/pages/result.php
+++ efteling/pages/result.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * User: William Steenbergen, Arne Kraaijeveld en Jesse van der Pluim
- * Date: 25-3-2024
- * File: Vragenlijst.php
+
+User: William Steenbergen, Arne Kraaijeveld en Jesse van der Pluim
+Date: 25-3-2024
+File: Vragenlijst.php
  */
 
 $indexNav= "../";
@@ -14,38 +15,55 @@ $images = "../images/";
 
 include "../includes/Header.php";
 ?>
-    <body>
-        <?php
-        $totaalPunten = 0;
+<body>
+<?php
+//gebruiker komt van het formulier
+if (isset($_POST["naam"]))
+{
+    $totaalPunten = 0;
+    for ($i = 1; $i <= 12; $i++)
+    {
+        $totaalPunten += $_POST["vraag$i"];
+    }
 
-        for($i=1; $i<=12; $i++)
-        {
-            $totaalPunten += $_POST["vraag$i"];
-        }
+    $naam = $_POST["naam"];
 
-        $naam = $_POST["naam"];
+    session_start();
+    $_SESSION['totaalPunten'] = $totaalPunten;
+    $_SESSION['naam'] = $naam;
+}
+else
+    { // gebruiker gaat rechtstreeks naar de resultatenpagina, staan er gegevens in de sessie?
+        session_start();
+        $totaalPunten = $_SESSION['totaalPunten'];
+        $naam = $_SESSION['naam'];
 
-        echo "<p class='resultaat'>$naam, je hebt $totaalPunten van de 120 punten behaald!</p>";
 
-        switch($totaalPunten)
-        {
-            case($totaalPunten > 100):
-                echo "<p class='resultaat'> Je houdt van snelle spannende attracties</p>";
-                break;
-            case($totaalPunten > 70 && $totaalPunten <= 100):
-                echo "<p class='resultaat'> je verzorgt je gebit goed, maar je op specifieke onderdelen moet je gedrag toch verbeteren</p>";
-                break;
-            case($totaalPunten > 30 && $totaalPunten <= 70):
-                echo "<p class='resultaat'> je verzorgt gebit matig en zou om advies moeten gaan vragen bij de tandarts. Waarschijnlijk heb je al gebitsproblemen?</p>";
-                break;
-            case($totaalPunten < 30):
-                echo "<p class='resultaat'> je verzorgt gebit niet en zou om advies moet gaan vragen bij de tandarts. 
-                      Waarschijnlijk ga je niet langs een tandarts en heb je ook al gebitsproblemen.</p>";
-                break;
-            default:
-                echo "<p class='resultaat'> Geen punten bekend</p>";
-        }
-        include "../includes/Footer.php"
-        ?>
-    </body>
+
+
+    }
+echo "<p class='resultaat'>$naam, je hebt $totaalPunten van de 120 punten behaald!</p>";
+
+switch($totaalPunten)
+{
+    case($totaalPunten > 100):
+        echo "<p class='resultaat'> Je houdt van snelle spannende attracties de baron is jouw favoriete atractie!</p> .<img src='../images/baron.jpg'>";
+
+        break;
+    case($totaalPunten > 70 && $totaalPunten <= 100):
+        echo "<p class='resultaat'> je houdt van snelle achtbanen maar niet van de Baron dan is de Python iets voor jou!</p> .<img src='../images/python.jpg' class='fotoresultaat'>";
+        break;
+    case($totaalPunten > 30 && $totaalPunten <= 70):
+        echo "<p class='resultaat'> Je houdt wel van een beetje actie maar je vind de Python nog een beetje eng? Dan is Max en Morritz iets voor jou!</p> . <img src='../images/maxenmoritz.jpg' class='fotoresultaat'>";
+        break;
+    case($totaalPunten < 30):
+        echo "<p class='resultaat'> je houdt eerder van een betoverende ervaring dan een snelle de droomvlucht is jouw ideale attractie!</p> . <img src='../images/droomvlucht.jpg' class='fotoresultaat'>";
+        break;
+    default:
+        echo "<p class='resultaat'> Oeps, het lijkt erop dat je niks hebt ingevuld?</p>. <img src='../images/langnek.jpg' class='fotoresultaat'>";
+}
+include "../includes/Footer.php";
+
+?>
+</body>
 </html>
